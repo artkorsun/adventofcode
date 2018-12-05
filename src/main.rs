bc687cd2af257dc99e4290b96e8fd2a5a31922e7
Artem Korsun <artem.korsun@gmail.com>
2018-12-05 20:08:40 +0100

    Day 2 Part 2

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,43 @@
 use std::io;
 use std::io::prelude::*;
 use std::fs::File;
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 fn main() -> io::Result<()> {
-    let mut f = File::open("src/day_1.data")?;
+    let mut f = File::open("src/day_2.data")?;
+    //let mut f = File::open("src/test.data")?;
     let mut buffer = String::new();
 
     f.read_to_string(&mut buffer)?;
 
-    let mut vals = HashSet::new();
+    let mut with_2 = 0;
+    let mut with_3 = 0;
 
-    let mut res = 0;
+    for s in buffer.lines() {
+        let mut vals = HashMap::new();
+        for c in s.chars() {
+            
+            let val = vals.entry(c).or_insert( 0 );
+            *val += 1;
+        }
 
-    loop {
+        let mut _2 = false;
+        let mut _3 = false;
 
-        for s in buffer.lines() {
-            let n:i32 = s.parse().unwrap();
+        for v in vals.values() {
+            _2 = _2 || *v == 2;
+            _3 = _3 || *v == 3;
+        }
 
-            res = res + n;
+        if _2 {
+            with_2 += 1;
+        }
 
-            if !vals.insert( res ) {
-                println!("{}", res);
-                return Ok(());
-            }
+        if _3 {
+            with_3 += 1;
         }
     }
+
+    println!("{}", with_2 * with_3);
+    return Ok(());
 }
