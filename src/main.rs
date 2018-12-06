e75f2eb26adebe08916f09162cbbbaceae9127a0
Artem Korsun <artem.korsun@here.com>
2018-12-06 11:49:41 +0100

    Add day 2 part 2

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io;
 use std::io::prelude::*;
 
 fn main() -> io::Result<()> {
-    let mut f = File::open("src/test.data")?;
+    let mut f = File::open("src/day_2.data")?;
     let mut buffer = String::new();
 
     f.read_to_string(&mut buffer)?;
@@ -25,12 +25,7 @@ fn main() -> io::Result<()> {
 
         let first_line = first_line.unwrap();
 
-        println!("Inspected string is '{}'", first_line );
-
         'outer: for s in temp_iterator {
-
-            println!("  Processed string is '{}'", s );
-
             let mut res = String::new();
             let mut mismatch_found = false;
             for c in first_line.char_indices() {
@@ -38,23 +33,12 @@ fn main() -> io::Result<()> {
 
                 let char_in_str = s[i..i+1].chars().next();
 
-                if char_in_str.is_some() {
-                    println!("      Char by index {} in inspected string '{}', in processed string '{}'", c.0, c.1, char_in_str.unwrap() );
-                }
-                else
-                {
-                    println!("      Char is not found in processed string by index {} in inspected string '{}'", c.0, c.1 );
-                }
-
                 if c.1 == char_in_str.unwrap() {
                     res.push(c.1);
-                    println!("          Chars '{}' and '{}' mathced, current res is {}, origin string '{}', processed string '{}'", c.1, char_in_str.unwrap(), res, first_line, s );
                 } else {
                     if mismatch_found {
-                        println!("          Second mismatch for chars '{}' and '{}' found, go to outer loop, current res is {}, origin string '{}', processed string '{}'", c.1, char_in_str.unwrap(), res, first_line, s );
                         continue 'outer;
                     } else {
-                        println!("          First mismatch for chars '{}' and '{}' found, continue processing, current res is {}, origin string '{}', processed string '{}'", c.1, char_in_str.unwrap(), res, first_line, s );
                         mismatch_found = true;
                         continue;
                     }
