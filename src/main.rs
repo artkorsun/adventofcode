4ce7d152fedbbc617081db4ec9eab0552cafc74e
Artem Korsun <artem.korsun@here.com>
2018-12-11 14:57:25 +0100

    Day 3 part 1

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use regex::Regex;
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
+use std::cmp;
 
 #[derive(Debug)]
 struct Rect {
@@ -27,6 +28,34 @@ fn parse_rect(data: &str) -> Rect {
     }
 }
 
+impl Rect {
+    fn intersect(&self, other: Rect) -> Option<Rect> {
+
+        if self.left + self.width <= other.left {
+            return None;
+        }
+
+        if other.left + other.width <= self.left {
+            return None;
+        }
+
+        if self.top + self.height <= other.top {
+            return None;
+        }
+
+        if other.top + other.height <= self.top {
+            return None;
+        }
+
+        let left = cmp::max( self.left, other.left );
+        let top = cmp::max( self.top, other.top );
+        let width = cmp::min( self.left + self.width - left, other.left + other.width - left );
+        let height =  cmp::min( self.top + self.height - top, other.top + other.height - top);
+
+        Some( Rect{ left, top, width, height } )
+    }
+}
+
 fn main() -> io::Result<()> {
     let mut f = File::open("src/test.data")?;
     let mut buffer = String::new();
@@ -36,12 +65,13 @@ fn main() -> io::Result<()> {
     let mut lines_iterator = buffer.lines();
     let mut temp_iterator;
 
+    let mut intersects = Vec::new();
+
     loop {
         let first_line = lines_iterator.next();
 
         if first_line.is_none() {
-            println!("Nothing found");
-            return Ok(());
+            break;
         }
 
         temp_iterator = lines_iterator.clone();
@@ -53,10 +83,17 @@ fn main() -> io::Result<()> {
         println!("Current rect: {:?}", rect);
 
         for s in temp_iterator {
-            let rect_to_process = parse_rect(&s);
+            let rect_2 = parse_rect(&s);
 
-            println!("  Processed rect: {:?}", rect_to_process);
+            match rect.intersect( rect_2 ) {
+                Some( r ) => intersects.push( r ),
+                None => continue
+            }
         }
+    }
+
+    for r in intersects {
+        println!("  Processed rect: {:?}", r);
     }
 
     return Ok(());
