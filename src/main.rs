226ab717b5b272c1cce8ad53617437a5be2b3fa5
Artem Korsun <artem.korsun@here.com>
2018-12-12 11:16:47 +0100

    Day 3 part 2

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use std::io::prelude::*;
 
 #[derive(Debug)]
 struct Rect {
+    id: u32,
     left: u32,
     top: u32,
     width: u32,
@@ -15,14 +16,15 @@ struct Rect {
 }
 
 fn parse_rect(data: &str) -> Rect {
-    let re = Regex::new(r".*@\s+(\d+),(\d+):\s+(\d+)x(\d+)").unwrap();
+    let re = Regex::new(r"#(\d+)\s+.*@\s+(\d+),(\d+):\s+(\d+)x(\d+)").unwrap();
 
     match re.captures(data) {
         Some(caps) => Rect {
-            left: caps[1].parse().unwrap(),
-            top: caps[2].parse().unwrap(),
-            width: caps[3].parse().unwrap(),
-            height: caps[4].parse().unwrap(),
+            id: caps[1].parse().unwrap(),
+            left: caps[2].parse().unwrap(),
+            top: caps[3].parse().unwrap(),
+            width: caps[4].parse().unwrap(),
+            height: caps[5].parse().unwrap(),
         },
         None => panic!(),
     }
@@ -55,6 +57,7 @@ impl Rect {
         let height = cmp::min(self.top + self.height - top, other.top + other.height - top);
 
         Some(Rect {
+            id: 0,
             left,
             top,
             width,
@@ -76,65 +79,23 @@ fn main() -> io::Result<()> {
         rects.push(rect);
     }
 
-    let mut intersects = Vec::new();
-
-    let mut left = std::u32::MAX;
-    let mut right = 0;
-    let mut top = std::u32::MAX;
-    let mut bottom = 0;
-
-    let mut rect_iterator = rects.iter();
-    let mut temp_iterator;
-
     loop {
-        let rect_1 = rect_iterator.next();
+        let rect = rects.pop().unwrap();
 
-        if rect_1.is_none() {
+        let len = rects.len();
+
+        if len == 0 {
+            println!("Result: {}", rect.id);
             break;
         }
 
-        temp_iterator = rect_iterator.clone();
+        rects.retain(|ref r| rect.intersect(r).is_none());
 
-        let rect_1 = rect_1.unwrap();
-
-        for rect_2 in temp_iterator {
-            match rect_1.intersect(rect_2) {
-                Some(r) => {
-                    left = cmp::min(left, r.left);
-                    right = cmp::max(right, r.left + r.width);
-                    top = cmp::min(top, r.top);
-                    bottom = cmp::max(bottom, r.top + r.height);
-
-                    intersects.push(r)
-                }
-                None => continue,
-            }
+        if rects.len() == len {
+            println!("Result: {}", rect.id);
+            break;
         }
     }
-
-    let mut matrix = vec![vec![0u8; (bottom - top) as usize]; (right - left) as usize];
-
-    for r in intersects {
-        let relative_left = r.left - left;
-        let relative_top = r.top - top;
-
-        for x in relative_left..relative_left + r.width {
-            for y in relative_top..relative_top + r.height {
-                matrix[x as usize][y as usize] = 1;
-            }
-        }
-    }
-
-    let mut count = 0;
-    for i in matrix {
-        for j in i {
-            if j == 1 {
-                count += 1;
-            }
-        }
-    }
-
-    println!("res: {:?}", count);
 
     return Ok(());
 }
