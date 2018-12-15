ba5a8e3f6b670c137059803e0ac96a6e9757c5d9
Artem Korsun <artem.korsun@gmail.com>
2018-12-15 10:49:10 +0100

    Day 5 part 1

--- day_5/part_1/src/main.rs
+++ day_5/part_1/src/main.rs
@@ -1,5 +1,25 @@
-use std::io;
+fn main() {
+    let input = include_str!("input.data");
+    let mut res = String::new();
 
-fn main() -> io::Result<()> {
-    return Ok(());
+    for c in input.chars() {
+        
+        if res.is_empty() {
+            res.push(c);
+            continue;
+        }
+
+        let last_char = res.pop().unwrap();
+
+        if last_char != c && last_char.to_ascii_uppercase() == c.to_ascii_uppercase() {
+            continue;
+        }
+        else
+        {
+            res.push(last_char);
+            res.push(c);
+        }
+    }
+
+    print!("{}", res.len());
 }
