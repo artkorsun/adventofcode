0f522b251e972fbbbcd88d6acf58fd88caabb673
Artem Korsun <artem.korsun@gmail.com>
2018-12-18 07:48:45 +0100

    Day 6 part 1

--- day_6/part_1/src/main.rs
+++ day_6/part_1/src/main.rs
@@ -11,21 +11,6 @@ struct Point {
     y: usize,
 } 
 
-#[derive(Debug, Clone)]
-enum Cell {
-    Empty,
-    Point {
-        index: usize,
-    },
-    Distance {
-        index: usize,
-        distance: usize,
-    },
-    Shared {
-        distance: usize,
-    },
-}
-
 fn parse_point(record: &str) -> Point {
     
     let re = Regex::new(r"(\d+),\s+(\d+)").unwrap();
@@ -39,13 +24,13 @@ fn parse_point(record: &str) -> Point {
     }
 }
 
-fn calc_distance(x0: i8, y0: i8, x1: i8, y1: i8) -> usize {
+fn calc_distance(x0: i32, x1: i32, y0: i32, y1: i32) -> usize {
     
     (x0 - x1).abs() as usize + (y0 - y1).abs() as usize
 }
 
 fn main() {
-    let input = include_str!("test.data");
+    let input = include_str!("input.data");
     
     let mut points = Vec::with_capacity(input.len());
 
@@ -65,74 +50,42 @@ fn main() {
         points.push(point);
     }
 
-    let mut matrix = vec![vec![Cell::Empty{};bottom_right.y - left_top.y + 1];bottom_right.x - left_top.x + 1];
+    let mut matrix = vec![vec![None;bottom_right.y - left_top.y + 1];bottom_right.x - left_top.x + 1];
     
     'col: for x in 0..bottom_right.x - left_top.x + 1{
         'row: for y in 0..bottom_right.y - left_top.y + 1{
+            let mut point_per_distance = HashMap::new();
+
             for (i, p) in points.iter().enumerate() {
                 
                 if x == p.x - left_top.x && y == p.y - left_top.y {
-                    matrix[x][y] = Cell::Point{index: i};
+                    matrix[x][y] = Some(i);
                     continue 'row;
                 }
 
-// 1, 1
-// 1, 6
-// 8, 3
-// 3, 4
-// 5, 5
-// 8, 9
-                let dist = calc_distance(x as i8, (p.x - left_top.x) as i8, y as i8, (p.y - left_top.y) as i8);
+                let dist = calc_distance(x as i32, (p.x - left_top.x) as i32, y as i32, (p.y - left_top.y) as i32);
+                let points = point_per_distance.entry(dist).or_insert(vec![]);
+                points.push(i);
+            }
 
-                match matrix[x][y] {
-                    Cell::Point{ index: _ } => {
-                        continue 'row;
-                    },
-                    Cell::Distance{ index: _, distance } => {
-                        if dist < distance {
-                            matrix[x][y] = Cell::Distance {
-                                                index: i,
-                                                distance: dist,
-                                            };    
-                        }
-                        if dist == distance {
-                            matrix[x][y] = Cell::Shared {
-                                                distance: dist,
-                                            };
-                        }
-                    },
-                    Cell::Shared{ distance } => {
-                        if dist < distance {
-                            matrix[x][y] = Cell::Distance {
-                                                index: i,
-                                                distance: dist,
-                                            };    
-                        }
-                    },
-                    Cell::Empty => {
-                        matrix[x][y] = Cell::Distance {
-                                                index: i,
-                                                distance: dist,
-                                            };
-                    },
-                    
-                    
-                }
+            let min_dist = point_per_distance.keys().min().unwrap();
+            let points_with_mit_distance = point_per_distance.get( min_dist ).unwrap();
+
+            if points_with_mit_distance.len() == 1 {
+                matrix[x][y] = Some(points_with_mit_distance[0]);
             }
         }
     }
-
-    print!("matrix: {:?}\n", matrix);
 
     let mut area_size_per_point = HashMap::new();
     let mut points_with_infinite_area = HashSet::new();
 
     for (x, col) in matrix.iter().enumerate() {
         for (y, _row) in col.iter().enumerate() {
-            
+
             match matrix[x][y] {
-                Cell::Distance{ index, distance: _ } => {
-                    let vals = area_size_per_point.entry(index).or_insert(0);
+                Some(index) => {
+                let vals = area_size_per_point.entry(index).or_insert(0);
                     *vals += 1;
 
                     if x == 0 || y == 0 || x == matrix.len() - 1 || y == col.len() - 1 {
@@ -143,9 +96,6 @@ fn main() {
             }
         }
     }
-
-    print!("area_size_per_point: {:?}\n", area_size_per_point);
-    print!("points_with_infinite_area: {:?}\n", points_with_infinite_area);
 
     let mut max_area = 0;
     for (i, area_size) in &area_size_per_point {
